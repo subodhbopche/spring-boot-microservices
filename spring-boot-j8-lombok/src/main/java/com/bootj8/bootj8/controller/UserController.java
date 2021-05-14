0598707c88c3425beaf7d93ccd80f4a980d6766f
subodhb <subodh.bopche@oaklandsystems.net>
2021-05-14 16:12:14 +0530

    add new swagger annotations

--- spring-boot-j8-lombok/src/main/java/com/bootj8/bootj8/controller/UserController.java
+++ spring-boot-j8-lombok/src/main/java/com/bootj8/bootj8/controller/UserController.java
@@ -3,6 +3,10 @@ package com.bootj8.bootj8.controller;
 
 import com.bootj8.bootj8.dto.UserDTO;
 import com.bootj8.bootj8.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+@Api(value = "UserController", description = "User and skills entity")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-
+    @ApiOperation(value = "return all persist user list", response = Iterable.class, tags = "/getUserList")
     @GetMapping
     public List<UserDTO> getUserList(){
         return userService.getAllUser();
@@ -28,7 +33,12 @@ public class UserController {
         userService.saveUser(userDTO);
     }
 
-
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success/OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found !!!")
+    })
     @GetMapping(value = "/{id}")
     public UserDTO getUser(@PathVariable Integer id){
         return userService.getUserByID(id);
