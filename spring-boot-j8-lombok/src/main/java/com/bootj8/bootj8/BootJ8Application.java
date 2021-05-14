975d3614eb44f62147c61a2909e6d1b15ab44e36
subodhb <subodh.bopche@oaklandsystems.net>
2021-05-14 09:18:58 +0530

    implement swagger for web service documents

--- spring-boot-j8-lombok/src/main/java/com/bootj8/bootj8/BootJ8Application.java
+++ spring-boot-j8-lombok/src/main/java/com/bootj8/bootj8/BootJ8Application.java
@@ -2,8 +2,10 @@ package com.bootj8.bootj8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BootJ8Application {
 
 	public static void main(String[] args) {
